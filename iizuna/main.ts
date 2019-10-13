05eff8f914448bb2224eee292b8a3bdd620a2f3f
nano1237 <tim.ruecker@ymail.com>
2019-10-13 17:17:11 +0200

    adding test template for the iizuna-page-transition component

--- iizuna/main.ts
+++ iizuna/main.ts
@@ -2,9 +2,18 @@ import {ComponentFactory} from "iizuna";
 import {LoadPriorityComponent, PageTransitionComponent} from "iizuna-page-transition";
 
 /**
- * Initializing all used Components
+ * The PageTransitionComponent has to cache all wanted components.
+ * That's why we have to pass them to the config method.
+ *
+ * The config method returns the passed components.
+ * @type {AbstractComponent[]}
  */
-ComponentFactory.registerComponents([
+const COMPONENTS = PageTransitionComponent.config([
 	PageTransitionComponent,
 	LoadPriorityComponent
 ]);
+
+/**
+ * Initializing all used Components
+ */
+ComponentFactory.registerComponents(COMPONENTS);
