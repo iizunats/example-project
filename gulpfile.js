dd34c6f93e4ae20c1e2fb27b7bf812de70c5da9c
nano1237 <tim.ruecker@ymail.com>
2019-10-18 18:57:05 +0200

    adding sass build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-let {series, watch, dest} = require("gulp"),
+let {series, watch, dest, src} = require("gulp"),
 	browserify = require("browserify"),
 	source = require('vinyl-source-stream'),
 	tsify = require("tsify"),
 	glob = require('glob'),
 	uglify = require('gulp-uglify'),
-	buffer = require('vinyl-buffer');
+	buffer = require('vinyl-buffer'),
+	sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 
 // Build JS
@@ -27,11 +29,18 @@ function scripts () {
 	  .pipe(dest("public/fileadmin/build"));
 }
 
+function scss () {
+	return src('./scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(dest('public/fileadmin/build'));
+}
+
 // Default gulp task
 function defaultTask (cb) {
 	watch('iizuna/**/*.ts', series(scripts));
 	cb();
 }
 
+exports.sass = scss;
 exports.scripts = scripts;
 exports.default = series(defaultTask);
